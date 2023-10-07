58d6f941536ddb18138e17948978e45004bafdcc
Carl Adams <carl51690@gmail.com>
2023-10-07 18:45:04 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: 'sam',
+    streetAddress: '11 Broadway',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newobj = { ...obj };
+    newobj[key] = value;
+    return newobj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newobj = { ...obj };
+    delete newobj[key]
+    return newobj;
+}  
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj;
+}
